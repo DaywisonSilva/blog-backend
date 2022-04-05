918aac76cbdc980f551632700323a76fbbf69531
DaywisonSilva <daywisonsilvasou@gmail.com>
2022-04-04 22:51:50 -0300

    remove cors

--- server.js
+++ server.js
@@ -3,15 +3,9 @@ const path = require('path')
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
-const cors = require('cors')
 
 server.use(middlewares)
 server.use(router)
-server.use(
-  cors({
-    origin: 'http://seila.com'
-  })
-)
 server.listen(80, () => {
   console.log('JSON Server is running')
 })
