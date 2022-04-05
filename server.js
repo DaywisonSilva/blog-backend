50342f9a208e1ca2bebce19f36df9a14592ad445
DaywisonSilva <daywisonsilvasou@gmail.com>
2022-04-04 23:03:04 -0300

    add env port

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require('path')
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 8080
 
 server.use(middlewares)
 server.use(router)
-server.listen(8080, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
